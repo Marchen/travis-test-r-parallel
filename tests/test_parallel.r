446333b5afb77b6eba4e5a6ab6cb93970c7de9d1
Michio Oguro <michio_oguro@hotmail.com>
2019-07-03 12:30:23 +0900

    Update test.

--- tests/test_parallel.r
+++ tests/test_parallel.r
@@ -4,6 +4,7 @@ library(testthat)
 for (i in 1:10) {
     test_that(
         "Test making cluster.", {
+            cat(sprintf("Using %s cores.", i))
             expect_silent(test_ cl <- makeCluster(i))
             stopCluster(cl)
         }
