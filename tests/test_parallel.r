723fcbb416a67476f882f87e711eafb98d9c9c4a
Michio Oguro <michio_oguro@hotmail.com>
2019-07-03 13:17:10 +0900

    Update test.

--- tests/test_parallel.r
+++ tests/test_parallel.r
@@ -2,6 +2,7 @@ library(parallel)
 library(testthat)
 
 for (i in 1:10) {
+    print(parallel::detectCores())
     test_that(
         "Test making cluster.", {
             cat(sprintf("Using %s cores.\n", i))
